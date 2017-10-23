3a99a6bdc3230164c60cc48376e58c26575f00ae
Brent Manzo <manzobrent@yahoo.com>
2017-10-23 13:58:02 +0800

    dbconnect

--- src/com/webapp/servlet/login.java
+++ src/com/webapp/servlet/login.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * Created by student on 10/20/2017.
@@ -25,6 +30,27 @@ public class login extends HttpServlet {
         }
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        try {
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            String url = "jdbc:mysql://localhost/webapp";
+            String username = "root";
+            String password = "";
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM user");
+            while (rs.next()){
+                out.println(rs.getString(1));
+                out.println(rs.getString(2));
+                out.println(rs.getString(3));
+            }
+            st.close();
+            rs.close();
+            conn.close();
 
+        }catch (Exception e){
+            out.println("Driver cannot be found...");
+        }
     }
 }
